9d6c86489a0ade030b7db9a843012405b222caf8
Veiledtechie <Veiledtechie@users.noreply.github.com>
2024-06-20 18:36:20 +0100

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
 import React from "react";
 import "./App.css";
-
+import Weather from "./Weather";
 function App() {
   return (
     <div className="App">
-      <header>
-        <h1>FindmyWeather</h1>
-      </header>
-
-      <footer>
-        This website was built by{" "}
-        <a
-          href="https://www.linkedin.com/in/hikmah-adetunji-325011250"
-          target="_blank"
-        >
-          Hikmah Adetunji
-        </a>{" "}
-        and is open-sourced on{" "}
-        <a href="" target="_blank">
-          Github
-        </a>
-      </footer>
+      <div className="container">
+        <header>
+          <h1>FindmyWeather</h1>
+        </header>
+        <Weather />
+        <footer>
+          This website was built by{" "}
+          <a
+            href="https://www.linkedin.com/in/hikmah-adetunji-325011250"
+            target="_blank"
+          >
+            Hikmah Adetunji
+          </a>{" "}
+          and is open-sourced on{" "}
+          <a
+            href="https://github.com/Veiledtechie/findmyweather"
+            target="_blank"
+          >
+            Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
---
+++ src/Weather.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./App.css";
+export default function Weather() {
+  return (
+    <div>
+      <h1></h1>
+    </div>
+  );
+}
