cfef8811e21bad441d9546299cd9c840667c3ec1
Hikmah Adetunji <hikmahadetunji20@gmail.com>
2026-08-28 11:04:09 +0100

    Update weatherforecast.js

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import WeatherIcon from "./WeatherIcon";
 import WeatherforecastDay from "./WeatherforecastDay";
 import "./WeatherForecast.css";
 export default function WeatherForecast(props) {
